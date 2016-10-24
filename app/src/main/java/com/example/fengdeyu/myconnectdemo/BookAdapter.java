c032e255316b7b7290a861fb312b4589de4d2f7a
aGoodBaby <827693352@qq.com>
2016-10-24 11:39:13 +0800

    添加搜索书籍功能(未完成/1)

---
+++ app/src/main/java/com/example/fengdeyu/myconnectdemo/BookAdapter.java
@@ -0,0 +1,68 @@
+package com.example.fengdeyu.myconnectdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by fengdeyu on 2016/10/24.
+ */
+
+public class BookAdapter extends BaseAdapter {
+    private List<BookBean> mList;
+    private LayoutInflater mInflater;
+
+    public BookAdapter(Context context, List<BookBean> mList) {
+        this.mList = mList;
+        mInflater=LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder=null;
+        if(convertView==null){
+            viewHolder=new ViewHolder();
+            convertView=mInflater.inflate(R.layout.book_item_layout,null);
+            viewHolder.ivIcon= (ImageView) convertView.findViewById(R.id.iv_icon);
+            viewHolder.tvTitle= (TextView) convertView.findViewById(R.id.tv_title);
+            viewHolder.tvContent= (TextView) convertView.findViewById(R.id.tv_content);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder= (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.ivIcon.setImageResource(R.mipmap.ic_launcher);
+        viewHolder.tvTitle.setText(mList.get(position).bookTitle);
+        viewHolder.tvContent.setText(mList.get(position).bookContent);
+
+
+        return convertView;
+    }
+
+    class ViewHolder{
+        public TextView tvTitle,tvContent;
+        public ImageView ivIcon;
+    }
+
+}
---
+++ app/src/main/java/com/example/fengdeyu/myconnectdemo/BookBean.java
@@ -0,0 +1,11 @@
+package com.example.fengdeyu.myconnectdemo;
+
+/**
+ * Created by fengdeyu on 2016/10/24.
+ */
+
+public class BookBean {
+    public String bookIconUrl;
+    public String bookTitle;
+    public String bookContent;
+}
